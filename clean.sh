ab66bba3e4da7ae270b3b990f1199f865ae645b6
HuangKangJing <huangkangjing@gmail.com>
2015-03-18 22:31:30 +0800

    Added clean.sh

---
+++ clean.sh
@@ -0,0 +1,8 @@
+find . -name '*.aux' -o \
+-name '*.log' -o \
+-name '*.out' -o \
+-name '*.bbl' -o \
+-name '*.blg' -o \
+-name '*.fdb_latexmk' -o \
+-name '*.fls' -o \
+-name 'textNotes.bib' -delete
